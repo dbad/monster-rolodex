e1d3c5e39feee943c36c2d26885d6c9a3b32ab8d
Daniel Badila <dv.badila@gmail.com>
2019-08-08 17:43:36 -0400

    Updates

---
+++ precache-manifest.105ce094311edad8e54c26d3e4a42aa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e386178edb88bbb4b4edd4c1e7a4ee",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b4fcf05e23917ea8a546",
+    "url": "/monster-rolodex/static/css/main.8116980e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b4fcf05e23917ea8a546",
+    "url": "/monster-rolodex/static/js/main.5cb5861b.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
